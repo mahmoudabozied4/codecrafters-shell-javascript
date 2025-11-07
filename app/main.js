ac44bf0a26e51dd9066ab594d2182f292dfd7819
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-11-07 20:26:17 +0200

    Implement a REPL

--- app/main.js
+++ app/main.js
@@ -6,7 +6,11 @@ const rl = readline.createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
-    console.log(`${answer}: command not found`) ;
-    rl.close();
-});
+function prompt () {
+    rl.question("$ ", (answer) => {
+        console.log(`${answer}: command not found`) ;
+        prompt();
+    });
+}
+
+prompt();
