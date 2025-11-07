e00a5f77a704eb94576607c55783d47df4eacbb2
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-11-07 21:01:24 +0200

    Implement type

--- app/main.js
+++ app/main.js
@@ -22,6 +22,17 @@ function prompt() {
             return;
         }
 
+        // type commend
+        if (ans.startsWith("type ")) {
+            if (ans.slice(5) === "echo" || ans.slice(5) === "exit" || ans.slice(5) === "type") {
+                console.log(`${ans.slice(5)} is a shell builtin`);
+            }else {
+                console.log(`${ans.slice(5)}: not found`);
+            }
+            prompt();
+            return;
+        }
+
         // unknown command
         if (ans !== "") {
             console.log(`${ans}: command not found`);
