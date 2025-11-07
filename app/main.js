675c2776bfbdb7a62c607690d7e893f9a2c0d0d8
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-11-07 19:49:05 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 // TODO: Uncomment the code below to pass the first stage
 rl.question("$ ", (answer) => {
-  rl.close();
+    console.log(answer , 'command not found') ;
+    rl.close();
 });
