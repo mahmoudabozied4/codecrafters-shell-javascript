9992f4fff4de89ec0684b3e59ffcf9283e229b91
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-11-07 21:49:05 +0200

    Implement type

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -26,30 +28,39 @@ function prompt() {
         }
 
         // exit command
-        if (ans === "exit 0") {
+        if (ans === "exit" || ans === "exit 0") {
             rl.close();
             return;
         }
 
-        // type commend
+        // type command
         if (ans.startsWith("type ")) {
             const arg = ans.slice(5).trim();
+
             // Builtins
             if (["echo", "exit", "type"].includes(arg)) {
                 console.log(`${arg} is a shell builtin`);
+                prompt();
                 return;
             }
+
             const pathDirs = process.env.PATH.split(path.delimiter);
+
             // Search in PATH
             for (const dir of pathDirs) {
                 const fullPath = path.join(dir, arg);
                 if (fs.existsSync(fullPath) && isExecutable(fullPath)) {
                     console.log(`${arg} is ${fullPath}`);
+                    prompt();
                     return;
                 }
             }
+
             console.log(`${arg}: not found`);
+            prompt();
+            return;
         }
+
         // unknown command
         if (ans !== "") {
             console.log(`${ans}: command not found`);
@@ -58,5 +69,6 @@ function prompt() {
         prompt();
     });
 }
+
 
 prompt();
