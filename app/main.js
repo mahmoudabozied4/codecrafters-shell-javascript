28574616157af5e9d3940d904e0143e583def02c
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-11-07 20:35:05 +0200

    Implement exit

--- app/main.js
+++ app/main.js
@@ -8,6 +8,10 @@ const rl = readline.createInterface({
 // TODO: Uncomment the code below to pass the first stage
 function prompt () {
     rl.question("$ ", (answer) => {
+        if (answer === "exit 0") {
+            rl.close();
+            return;
+        }
         console.log(`${answer}: command not found`) ;
         prompt();
     });
